a5947ae7b8cd2aa60a49598e47ba49f131d2ee09
repl.it user <replituser@example.com>
2021-03-10 21:47:49 +0000

    Activity 2 for while loops

---
+++ main.py
@@ -0,0 +1,9 @@
+print("How many live cables must avoid?")
+cables_to_avoid=int(input())
+cables_avoided=0
+#print()
+while(cables_avoided<cables_to_avoid):
+ print("Avoiding...", end="")
+ cables_avoided=cables_avoided+1
+ print("Done!",cables_avoided,"cables avoided.")
+
